06141b5d05e5e859688b895e292eb7f35e64421f
sweetstyle11 <ksandage08@gmail.com>
2016-01-14 01:50:11 +0000

    Done

--- lib/find_non_repeated_letter.rb
+++ lib/find_non_repeated_letter.rb
@@ -9,19 +9,14 @@
 
 
 def find_first_non_repeated_letter(str)
-array = str.split(//)
-counts = Hash.new 0  #creates new hash 
+  array = str.split(//)
+  counts = Hash.new 0  #creates new hash 
 
-array.each do |word|
-  counts[word] += 1   #count the characters in array and put in hash with key = letter and value= the character's frequency
+  array.each do |x|
+    counts[x] += 1   #count the characters in array and put in hash with key = letter and value= the character's frequency
 end
 counts.delete_if{|key, value| value >= 2} #delete key, value pair if it occurs more than once
-counts.keys[0] #if there are more than 1 unique character need to return the first one in the string
+counts.keys[0]
 
 end
 
-find_first_non_repeated_letter("aaabbbc")
-find_first_non_repeated_letter("abbccddee")
-find_first_non_repeated_letter("aaabccd")
-find_first_non_repeated_letter("abcde")
-find_first_non_repeated_letter("aabbcc")
