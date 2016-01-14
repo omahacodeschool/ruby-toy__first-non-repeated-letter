22c723834ff6c2acc3fa4944c82283b18488ceb1
Melinda <melinda.deinert@gmail.com>
2016-01-14 00:36:40 +0000

    Done

--- lib/find_non_repeated_letter.rb
+++ lib/find_non_repeated_letter.rb
@@ -1,14 +1,14 @@
-# This method takes a string, str, and returns the first non-repeated letter in that string.
-# More specifically, it looks for the first letter that appears by itself.
-#
-# + "ddcdd" has 'c' as its first non-repeated letter, and thus returns 'c'
-# + "aabccd" has both 'b' and 'd' as non-repeating letters, but would return 'b' because that
-#   one occurs first
-# + "aabbaa" only has repeated letters (two pairs of 'a', and a pair of 'b'), and thus returns
-#   nil (since there does not exist a non-repeated letter)
-
-
 def find_first_non_repeated_letter(str)
-  #write your method here
-  return true
+    str.split('').each do |x|
+        str.count(x)
+        if str.count(x) == 1
+            return x
+        end
+    end
+    str.split('').each do |x|
+        str.count(x)
+        if str.count(x) > 1
+            return nil
+        end
+    end
 end
