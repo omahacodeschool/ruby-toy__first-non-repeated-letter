6a64929814b91cd0f19d90b3ee4f276378ad653b
bethanyhaubert <bethany.haubert@gmail.com>
2016-01-15 15:54:04 +0000

    wrote out thought process

--- lib/find_non_repeated_letter.rb
+++ lib/find_non_repeated_letter.rb
@@ -9,18 +9,27 @@
 
 
 def how_many_times_is_repeated(str)
+  #create hash to store key and value
+  h={}
+  #split up the string into individual characters and put it in an array
   arr = str.split("")
+  #for each character in the array, if it does not have a previous occurence in the 'h' hash, add it to the hash with a value of '1'
   arr.each do |n|
     if h.has_key?(n) == false
       h[n]=1
+  #if it does have a previous occurence in the 'h' hash, add '1' to the previously occuring value of that key
     elsif h.has_key?(n) == true
       h[n]= h[n] +1
     end
+    #return the completed hash
+    return h
 end
 
 def find_first_non_repeated_letter(str)
-    a = how_many_times_is_repeated(str)
-    #next line should create an array that stores the hash keys with a value of '1' from the hash created by the 'how many times is repeated' method
-    b = [a.hash.index(1)]
-    returns b(0)
+    #assign the above block inside this block
+    h = how_many_times_is_repeated(str)
+    #return the first hash hey with a value of '1' from the hash created by the 'how many times is repeated' method
+    #first_non_repeat = h.index(1)
+    #return first_non_repeat
+    return h.index(1)
 end
