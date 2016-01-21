f135077ceb1a1d01af7741027a5e3fae68780148
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-21 00:19:23 +0000

    Done

--- lib/find_non_repeated_letter.rb
+++ lib/find_non_repeated_letter.rb
@@ -8,24 +8,14 @@
 #   nil (since there does not exist a non-repeated letter)
 
 
-#Am I thinking about this correctly?
 
-def find_first_non_repeated_letter(str)
-    str = str.chars
-    
-#somehow group the chars array into array like below....group_by????
+#update: going down this path now. gotta put in a conditional and break the loop once the first non-repeating letter is found.
 
-a = ['aaa', 'bbb', 'c', 'ddd']
-max = 1
-a.each do |str| 
-    if str.length <= max
-        break puts str
-    else puts nil
-    end
-end
+def find_first_non_repeated_letter(str) 
+   str = str.chars
+   str.select { |char| str.count(char) <= 1 }
 
-end
 
-#i also have been fidiling with somthing like
-#str.scan(/[a-z]{1}/)
-#but if I am reading that correctly that means 1 of any character a-z so... the whole string returns. 
+end true
+
+
