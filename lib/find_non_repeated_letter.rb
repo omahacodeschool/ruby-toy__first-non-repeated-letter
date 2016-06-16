6e48c7bd6c742aa0aa6c5421ebd7ef37d6924e11
Matt Hughes <mhughes27@gmail.com>
2016-06-16 21:40:39 +0000

    Resubmitting using an array WIP

--- lib/find_non_repeated_letter.rb
+++ lib/find_non_repeated_letter.rb
@@ -9,16 +9,18 @@
 
 
 def find_first_non_repeated_letter(str)
-  
-  new_array = str.chars.to_a
-  new_hash = {}
-  
-  new_array.each do |x|
-    new_hash[x] = 0 if new_hash[x].nil?
-    new_hash[x] = new_hash[x] + 1
-  end
-  
-  first_nonrepeat = new_hash.key(1)
-  
-  return first_nonrepeat
+
+arr = str.split("")
+if arr[0] != arr[1] then return arr[0]
+end
+i = 0
+while i < arr.length
+if arr[i] != arr[i + 1] && arr[i + 1] != arr[i + 2]
+  return arr[i + 1]
+elsif arr[-2] != arr[-1]
+  return arr[-1]
+else
+  return nil
+end
+end
 end
